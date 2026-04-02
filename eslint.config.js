7002bbf7b4a2a8bfa3a7dc6e4d127cdc99f80718
Steve Montgomery <steve@chums.com>
2026-04-02 11:52:06 -0600

    updates: - npm updates & major version updates as required - updates to vite config, eslint config, tsconfig, etc. - updates to preferred structure in README.md - add light/dark mode toggle in header bar

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,33 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { globalIgnores, defineConfig } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
             js.configs.recommended,
             tseslint.configs.recommended,
-            reactHooks.configs['recommended-latest'],
+            reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
         ],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    "varsIgnorePattern": "^_",
+                    "ignoreRestSiblings": true,
+                    "caughtErrorsIgnorePattern": "^_"
+                }
+            ],
+        }
     },
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react';
 import path from "node:path";
 import process from "node:process";
+import {visualizer} from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
-export default defineConfig({
+const commonConfig = defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
             "@/api": path.resolve(process.cwd(), 'src/api'),
             "@/app": path.resolve(process.cwd(), 'src/app'),
             "@/components": path.resolve(process.cwd(), 'src/components'),
-            "@/ducks": path.resolve(process.cwd(), 'src/ducks'),
+            "@/features": path.resolve(process.cwd(), 'src/features'),
             "@/hooks": path.resolve(process.cwd(), 'src/hooks'),
-            "@/slices": path.resolve(process.cwd(), 'src/slices'),
-            "@/src": path.resolve(process.cwd(), 'src'),
             "@/types": path.resolve(process.cwd(), 'src/types'),
             "@/utils": path.resolve(process.cwd(), 'src/utils'),
         }
     },
-    base: "/apps/amz-fba-item-map/",
-    build: {
-        manifest: true,
-        sourcemap: true,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor'
-                    }
-                    if (id.includes('src/components')) {
-                        return 'components';
-                    }
-                }
-            }
-        }
-    },
+})
+
+const serverConfig = defineConfig({
     server: {
         port: 8080,
         host: 'localhost',
@@ -43,7 +27,86 @@ export default defineConfig({
             '/api': {
                 target: 'http://localhost:8081',
                 changeOrigin: true,
+            },
+            '/api/user/v2/cookie-consent.png': {
+                target: 'https://intranet.chums.com/api/user/v2/cookie-consent.png',
+                changeOrigin: true,
+            },
+            '/images': {
+                target: 'https://intranet.chums.com/',
+                changeOrigin: true,
             }
         }
     }
+})
+
+// https://vite.dev/config/
+export default defineConfig(({command, isSsrBuild}) => {
+    if (command === 'serve') {
+        return {
+            ...commonConfig,
+            ...serverConfig
+        }
+    }
+    if (isSsrBuild) {
+        return defineConfig({
+            ...commonConfig,
+            build: {
+                outDir: 'dist-server',
+                lib: {
+                    entry: path.resolve(process.cwd(), 'src/server/index.ts'),
+                    name: 'chums-ssr',
+                    formats: ['es'],
+                },
+                emptyOutDir: true,
+                rolldownOptions: {
+                    input: path.resolve(process.cwd(), './src/server/index.ts'),
+                    output: {
+                        preserveModules: true,
+                        // codeSplitting: false
+                    },
+                    shimMissingExports: true,
+                },
+                ssr: true,
+                ssrManifest: true,
+                ssrEmitAssets: true,
+                manifest: true,
+                sourcemap: false,
+            },
+            ssr: {
+                resolve: {
+                    conditions: ['development', 'browser'],
+                    externalConditions: ['node']
+                },
+                optimizeDeps: {
+                    holdUntilCrawlEnd: true,
+                }
+            }
+        })
+    }
+
+    return defineConfig({
+        ...commonConfig,
+        build: {
+            // minify: false,
+            outDir: 'dist-client',
+            emptyOutDir: true,
+            rolldownOptions: {
+                input: './src/client/index.tsx',
+                plugins: [visualizer({filename: 'stats.html', gzipSize: true})],
+                output: {
+                    codeSplitting: {
+                        groups: [
+                            {name: 'react', test: /node_modules\/(react|react-dom)/},
+                            {name: 'mui', test: /node_modules\/@mui/},
+                            {name: 'vendor', test: /node_modules/},
+                        ]
+                    }
+                }
+            },
+            manifest: true,
+            sourcemap: true,
+        },
+    })
+
 })
